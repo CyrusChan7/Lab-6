0388901a6139fbd5b20b5d518498ccec7acdb7b2
Cyrus Chan <cyruschan4561@gmail.com>
2020-11-25 21:47:32 -0800

    Fixed loop error, all lab requirements completed

--- PERSONAL_WEBSITE/js/script.js
+++ PERSONAL_WEBSITE/js/script.js
@@ -7,20 +7,24 @@ const courseList = [
 
 let userDigits = prompt("Please enter a 4-digit number:");
 
+let found = false;
 for (let i = 0; i < courseList.length; i++) {
     if (userDigits == courseList[i].id) {
         console.log("Yes I am taking the course: " + courseList[i].code + " - " + courseList[i].name);
-        break;
-    } else {
-        courseList.push( {code: userDigits, name: null, id: userDigits} );
-        console.log("New entry added successfully.");
-        break;
-    }
+        found = true;
+    } 
+}
+
+if (found == false) {
+    courseList.push( {code: userDigits, name: null, id: userDigits} );
+    console.log("New entry added successfully.");
 }
 
 
+/*
 // Comment out when not debugging
 console.log("Debug");
 for (let i = 0; i < courseList.length; i++) {
     console.log(courseList[i]);
 }
+*/
